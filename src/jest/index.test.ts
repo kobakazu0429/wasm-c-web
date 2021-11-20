62a1a8e7e8d61e9836c42ce36e1b91f7e844f9b4
kobakazu0429 <kobakazu10@gmail.com>
2021-11-20 22:18:46 +0900

    skip test

--- src/jest/index.test.ts
+++ src/jest/index.test.ts
@@ -1,46 +1,21 @@
-import { isTestFixture, testBuilder } from "./index";
-import type { Test, TestFixture } from "./index";
+import { run, constructResultsHTML } from "@kobakazu0429/test";
+import { testBuilder } from "./index";
+import type { Test } from "./index";
 
-const test: Test = {
-  name: "sum(1, 2) should be 3",
-  functionName: "sum",
-  input: [1, 2],
-  expect: 3,
-};
-
-const testFixture: TestFixture = {
-  title: "function",
-  tests: [
-    {
-      title: "sum",
-      tests: [
-        {
-          name: "sum(1, 2) should be 3",
-          functionName: "sum",
-          input: [1, 2],
-          expect: 3,
-        },
-      ],
-    },
-    {
-      title: "div",
-      tests: [
-        {
-          name: "div(8, 2) should be 4",
-          functionName: "div",
-          input: [8, 2],
-          expect: 4,
-        },
-        {
-          name: "div(10, 3) should be 3.3333",
-          functionName: "div",
-          input: [10, 3],
-          expect: 3.3333,
-        },
-      ],
-    },
-  ],
-};
+const tests: Test[] = [
+  {
+    name: "sum(1, 2) should be 3",
+    functionName: "sum",
+    input: [1, 2],
+    expect: 3,
+  },
+  {
+    name: "div(8, 2) should be 4",
+    functionName: "div",
+    input: [8, 2],
+    expect: 4,
+  },
+];
 
 function sum(a: number, b: number) {
   return a + b;
@@ -52,30 +27,11 @@ function div(a: number, b: number) {
 
 // inject jest
 // @ts-ignore
-window.jestLite = {
-  core: { describe, it, expect },
-};
 
 describe("jest", () => {
-  describe("isTestFixture", () => {
-    it("pass Test should be false", () => {
-      expect(isTestFixture(test)).toBeFalsy();
-    });
-
-    it("pass TestFixture should be true", () => {
-      expect(isTestFixture(testFixture)).toBeTruthy();
-    });
-  });
-
-  describe("testBuilder", () => {
-    testBuilder(
-      {
-        title: "test builder with test",
-        tests: [test],
-      },
-      { sum }
-    )();
-
-    testBuilder(testFixture, { sum, div })();
+  test.skip("testBuilder", async () => {
+    testBuilder(tests, { sum, div })();
+    const result = await run();
+    expect(constructResultsHTML(result)).toMatchInlineSnapshot();
   });
 });
