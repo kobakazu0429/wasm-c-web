41b654cdb32050ede153937820df8e1375401984
kobakazu0429 <kobakazu10@gmail.com>
2021-12-15 10:41:10 +0900

    feat: impl updateUrlParams

--- src/url.ts
+++ src/url.ts
@@ -10,4 +10,17 @@ export const rewriteUrlParams = (params: Array<["data", string]>) => {
   navigate(`/?${params.map((p) => p.join("=")).join("&")}`);
 };
 
+export const updateUrlParams = (params: Partial<RecoveryCode>) => {
+  const currentParams = new URLSearchParams(window.location.search);
+  const currentLzString = currentParams.get("data");
+  if (currentLzString) {
+    const currentLz: Partial<RecoveryCode> = JSON.parse(currentLzString);
+    const updateParams = compressLzString(
+      JSON.stringify({ ...currentLz, ...params })
+    );
+    rewriteUrlParams([["data", updateParams]]);
+  } else {
+    const updateParams = compressLzString(JSON.stringify(params));
+    rewriteUrlParams([["data", updateParams]]);
+  }
 };
