169d1d32590acef83fe66c09ecd64458687f2bb0
kobakazu0429 <kobakazu10@gmail.com>
2022-01-18 00:56:56 +0900

    fix type

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,5 @@
 import { run, constructResultsHTML } from "@kobakazu0429/test";
-import { testBuilder } from "./index";
+// import { testBuilder } from "./index";
 import type { Test } from "./index";
 
 const tests: Test[] = [
@@ -7,31 +7,37 @@ const tests: Test[] = [
     id: "1",
     testName: "sum(1, 2) should be 3",
     functionName: "sum",
-    argumentsValue: [1, 2],
-    returnValue: 3,
+    argumentsValues: [
+      { type: "int", value: 1 },
+      { type: "int", value: 2 },
+    ],
+    returnValue: { type: "int", value: 3 },
     returnPrecision: 0,
   },
   {
     id: "2",
     testName: "div(8, 2) should be 4",
     functionName: "div",
-    argumentsValue: [8, 2],
-    returnValue: 4,
+    argumentsValues: [
+      { type: "int", value: 8 },
+      { type: "int", value: 2 },
+    ],
+    returnValue: { type: "int", value: 4 },
     returnPrecision: 0,
   },
 ];
 
-function sum(a: number, b: number) {
-  return a + b;
-}
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
 
-function div(a: number, b: number) {
-  return a / b;
-}
+// function div(a: number, b: number) {
+//   return a / b;
+// }
 
 describe("jest", () => {
   test.skip("testBuilder", async () => {
-    testBuilder(tests, { sum, div })();
+    // testBuilder(tests, { sum, div })();
     const result = await run();
     expect(constructResultsHTML(result)).toMatchInlineSnapshot();
   });
