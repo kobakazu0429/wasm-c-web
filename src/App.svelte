aab56a1009460c41876ef8a6c31b507407cac63d
kobakazu0429 <kobakazu10@gmail.com>
2022-01-18 23:51:48 +0900

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Router, Route } from "svelte-routing";
   import Loadable from "svelte-loadable";
+  import Index from "./pages/index.svelte";
 </script>
 
 <Router>
@@ -8,6 +9,7 @@
     <Loadable loader={() => import("./pages/admin.svelte")} />
   </Route>
   <Route path="/" let:location>
-    <Loadable loader={() => import("./pages/index.svelte")} {location} />
+    <!-- <Loadable loader={() => import("./pages/index.svelte")} {location} /> -->
+      <Index {location} />
   </Route>
 </Router>
