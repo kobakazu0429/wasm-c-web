b4c82ad0e931440afe398ae7886b82eca61389fa
kobakazu0429 <kobakazu10@gmail.com>
2022-01-18 12:40:05 +0900

    split chunk page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
-<script>
+<script lang="ts">
   import { Router, Route } from "svelte-routing";
-  import Index from "./pages/index.svelte";
-  import Admin from "./pages/admin.svelte";
+
+  let Index: any;
+  let Admin: any;
+
+  import("./pages/index.svelte").then((m) => (Index = m.default));
+  import("./pages/admin.svelte").then((m) => (Admin = m.default));
 </script>
 
 <Router>
-  <Route path="/" let:location><Index {location} /></Route>
-  <Route path="/admin"><Admin /></Route>
+  <Route path="/" let:location>
+    {#if Admin}
+      <svelte:component this={Index} {location} />
+    {/if}
+  </Route>
+  <Route path="/admin">
+    {#if Admin}
+      <svelte:component this={Admin} />
+    {/if}
+  </Route>
 </Router>
