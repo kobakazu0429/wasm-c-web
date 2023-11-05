a80f0ae8ce43113dba4dd49498d00e1f98cf6273
kobakazu0429 <kobakazu10@gmail.com>
2023-11-06 02:33:07 +0900

    remove unnecessary env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,5 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-interface ImportMetaEnv {
-  readonly VITE_API_SERVER_URL: string;
-  readonly VITE_API_SERVER_DOMAIN: string;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ImportMetaEnv {}
