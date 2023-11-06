ecef3f7e65c761bca705848998e0c7d0ae61a133
kobakazu0429 <kobakazu10@gmail.com>
2023-11-06 11:55:46 +0900

    replace: jest to vitest

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,6 @@
+import { describe, test, expect } from "vitest";
 import { run, constructResultsHTML } from "@kobakazu0429/test";
-// import { testBuilder } from "./index";
+import { testBuilder } from "./index";
 import type { Test } from "./index";
 
 const tests: Test[] = [
@@ -27,18 +28,90 @@ const tests: Test[] = [
   },
 ];
 
-// function add(a: number, b: number) {
-//   return a + b;
-// }
+function add(a: number, b: number) {
+  return a + b;
+}
 
-// function div(a: number, b: number) {
-//   return a / b;
-// }
+function div(a: number, b: number) {
+  return a / b;
+}
 
 describe("jest", () => {
-  test.skip("testBuilder", async () => {
-    // testBuilder(tests, { add, div })();
-    const result = await run();
-    expect(constructResultsHTML(result)).toMatchInlineSnapshot();
+  describe("testBuilder", () => {
+    const timeRegExp = /\s*Time: \d+.\d+ \[s\]\s*/g;
+    test.skip("PASS", async () => {
+      testBuilder(tests, { add, div }, new ArrayBuffer(0))();
+      const result = await run();
+      const html = constructResultsHTML(result).trim().replace(timeRegExp, "");
+      expect(html).toMatchInlineSnapshot(`
+        "<div class=\\"test-report__result\\">
+              <span class=\\"test-report__status-icon\\">✓</span>
+              <span class=\\"test-report__status test-report__status--pass\\">
+                PASS
+              </span>
+              add(1, 2) should be 3
+
+
+            </div>
+
+            <div class=\\"test-report__result\\">
+              <span class=\\"test-report__status-icon\\">✓</span>
+              <span class=\\"test-report__status test-report__status--pass\\">
+                PASS
+              </span>
+              div(8, 2) should be 4
+
+
+            </div>
+
+
+            <span class=\\"test-report__summary-status test-report__summary-status--pass\\">
+              Tests: 0 failed, 2 passed, 2 total<br></span>"
+      `);
+    });
+
+    test("FAIL", async () => {
+      testBuilder(
+        tests,
+        {
+          add: function add(a: number, b: number) {
+            return a - b;
+          },
+          div,
+        },
+        new ArrayBuffer(0)
+      )();
+      const result = await run();
+      const html = constructResultsHTML(result).replace(timeRegExp, "");
+      expect(html).toMatchInlineSnapshot(`
+        "
+            
+            <div class=\\"test-report__result\\">
+              <span class=\\"test-report__status-icon\\">×</span>
+              <span class=\\"test-report__status test-report__status--fail\\">
+                FAIL
+              </span>
+              add(1, 2) should be 3
+              <div class=\\"test-report__errors\\">expected: 3, received: -1</div>
+              
+            </div>
+          
+            <div class=\\"test-report__result\\">
+              <span class=\\"test-report__status-icon\\">✓</span>
+              <span class=\\"test-report__status test-report__status--pass\\">
+                PASS
+              </span>
+              div(8, 2) should be 4
+              
+              
+            </div>
+          
+            
+            <span class=\\"test-report__summary-status test-report__summary-status--fail\\">
+              Tests: 1 failed, 1 passed, 2 total<br></span>
+          
+          "
+      `);
+    });
   });
 });
